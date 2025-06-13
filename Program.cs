2e112b2bdd5dfca7156547375baa26646720505b
RezaSh798 <rezakhorramisalarabadi@gmail.com>
2025-06-13 16:46:31 +0330

    Feature: Add swagger

--- Program.cs
+++ Program.cs
@@ -1,16 +1,28 @@
 using CQRSBookStore.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// builder.Services.AddDbContext<AppDbContext>(options =>
+//     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(opt => 
+    opt.UseInMemoryDatabase("CQRSBookStoreDB"));
 
 builder.Services.AddMediatR(cfg =>
     cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 
 builder.Services.AddControllers();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "CQRSBookStoreApi", Version = "v1" });
+});
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI(c => 
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookStoreApi v1");
+});
 app.MapControllers();
 app.Run();
